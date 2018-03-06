308237e4b73a5651561042475091f6be26c87931
Quyet <quyet.nguyen@aleph-labs.com>
2018-03-06 22:34:36 +0700

    add dynamic port

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const MongoClient = require('mongodb').MongoClient
 const bodyParser = require('body-parser')
 const db = require('./configs/db')
-const PORT = 8000
+const PORT = process.env.PORT || 8080
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }))
